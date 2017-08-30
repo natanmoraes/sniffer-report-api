84b6d176d2a43618ab14c813e5aa5649a74d1867
natanmoraes <natan.sbo@gmail.com>
2017-08-30 10:41:19 -0300

    Commit tmp changes

--- web/index.php
+++ web/index.php
@@ -13,7 +13,11 @@ use SnifferReport\Response\SnifferErrorResponse;
 use SnifferReport\Response\SnifferSuccessResponse;
 
 $app = new Application();
-$app['debug'] = true;
+
+$app->register(new \Silex\Provider\MonologServiceProvider(), [
+    'monolog.logfile' => '/tmp/sniffer-report.log',
+    'monolog.name' => 'sniffer-report',
+]);
 
 // Receive files to be sniffed.
 // @todo: Make different urls for git url and files
@@ -31,7 +35,9 @@ $app->error(function (SnifferReportException $e) use ($app) {
 
 // Generic error handler when something unexpected happens.
 $app->error(function (\Exception $e) use ($app) {
-    // @fixme: Add a log system with clear messages for debugging.
+    $app['monolog']->error('Unexpected error occurred', [
+        'exception' => print_r($e, 1),
+    ]);
     return new SnifferErrorResponse('An unexpected error occurred. Please contact the support.');
 });
 
